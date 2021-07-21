6b9ce2e454d62a81b5677c6349e8bee2f826e296
esthe <ko990727@hmail.com>
2021-07-21 10:14:44 +0900

    Init Change ViewFlipper Commit

--- app/src/main/java/kr/hs/emirim/ko502804/project10_2/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/ko502804/project10_2/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
-
+// Change ViewFlipper
 public class MainActivity extends AppCompatActivity {
     String[] imgNames = {"SEUNGMIN1", "SEUNGMIN2", "SEUNGMIN3", "SEUNGMIN4", "SEUNGMIN5", "SEUNGMIN6", "SEUNGMIN7", "SEUNGMIN8", "SEUNGMIN9"};
     int[] imgVIds = {R.id.img01, R.id.img02, R.id.img03, R.id.img04,R.id.img05, R.id.img06, R.id.img07, R.id.img08,R.id.img09};
